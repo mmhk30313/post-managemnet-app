7745a8e59f54fed7533d57d63624170b66be4bd7
Mehedi-Developer <mehedihasan30313@gmail.com>
2022-01-08 09:43:19 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,26 @@ function App() {
   return (
     <div className="d-flex position-relative">
         <div className='d-flex position-fixed w-100 h-100 border'>
-          <div style={{width: "25%"}} className="border">
-            <div className="p-5 text-center">
-              <button onClick={() => setIsTransition(!isTransition)} className='btn btn-outline-danger'>Open</button>
+          <div className={`${isTransition ? "side-nav auto" : "side-nav"} text-center border p-3`}>
+            <div style={{width: !isTransition ? "100%" : "25%"}} className="position-absolute">
+              <div className="p-5">
+                <button onClick={() => setIsTransition(!isTransition)} className='btn btn-outline-danger'>Open</button>
+              </div>
+              <div className="d-flex justify-content-around">
+                <button onClick={() => setContentStyle("grid")} className="btn btn-outline-primary">Grid</button>
+                <button onClick={() => setContentStyle("list")} className="btn mx-2 btn-outline-info">List</button>
+              </div>
             </div>
-            <div className="d-flex justify-content-center">
-              <button onClick={() => setContentStyle("grid")} className="btn btn-outline-primary">Grid</button>
-              <button onClick={() => setContentStyle("list")} className="btn mx-2 btn-outline-info">List</button>
-            </div>
+            
           </div>
-          <div className={`${isTransition ? "side-nav auto" : "side-nav"} border p-3`}>
+          {/* <div className={`${isTransition ? "side-nav auto" : "side-nav"} border p-3`}>
             <h2>Hello</h2>
             <h2>Hello</h2>
             <h2>Hello</h2>
             <h2>Hello</h2>
-          </div>
+          </div> */}
         </div>
-        <div style={{left: "21.5%", width: '69.5%', zIndex: !isTransition ? 1 : -1, transition: "850ms"}} className='position-absolute p-5 '>
+        <div style={{left: "21.5%", width: '69.5%', zIndex: !isTransition ? 1 : -1,}} className='position-absolute p-5 '>
             <div className={`m-4 row justify-content-center`}>
               {
                 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1].map(item => {
